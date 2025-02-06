71f425eebfa39571837d42d7d2caf283558ebafa
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-06 18:42:54 +0900

    Feat: Character Class 및 생성자 호출
    Character Class를 만들고, 매개변수(name, job, level, atk, def, maxHp, gold)를 받는 생성자를 만들었습니다.

---
+++ TextRPGTeam33/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTeam33
+{
+    public class Character
+    {
+        // 프로퍼티 생성
+        public int Level { get; }
+        public String Name { get; set; }
+        public string Job { get; }
+
+        
+        public int Attack { get; }
+        public int PlusAttack { get; set; }
+
+
+        public int Defense { get; }
+        public int PlusDefense { get; set; }
+
+        public int Hp { get; }
+        public int MaxHP { get; }
+        public int Gold { get; set; }
+
+        public int DungeonClearCount { get; set; }
+
+
+        
+        public Character(int level, string name, string job, int atk, int def, int maxHp, int gold)
+        {
+            Name = name;
+            Job = "전사";
+            Level = level;
+            Attack = atk;
+            PlusAttack = 0;
+            Defense = def;
+            PlusDefense = 0;
+            Gold = gold;
+            Hp = maxHp;
+            MaxHP = maxHp;
+            DungeonClearCount = 0;
+
+        }
+    }
+}
