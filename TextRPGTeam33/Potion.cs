26b1045811cb3b296bcdcf9fa29eb31bf6d893b9
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-07 15:40:10 +0900

    Revert "Remove: 전투 외 삭제"
    This reverts commit fe595b7f28d517a2d92b5be1d4837d660e2b6d72.

---
+++ TextRPGTeam33/Potion.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTeam33
+{
+    public class Potion
+    {
+        private List<Item> potions;
+        private Character character;
+
+        public Potion(Character player) 
+        {
+            this.character = player;
+        RefreshPotions();
+        }
+        private void RefreshPotions()
+        {
+            potions = character.Inventory.GetItems().Where(x => x.Type == ItemType.Potion && x.Count > 0).ToList();
+        }
+        public void DisplayPotion()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("회복");
+
+                RefreshPotions();  // 포션 목록 새로고침
+
+                if (potions.Count == 0)
+                {
+                    Console.WriteLine("사용 가능한 포션이 없습니다.\n");
+                }
+                else
+                {
+                    Console.WriteLine("[보유 포션 목록]");
+                    for (int i = 0; i < potions.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {potions[i].Name} | 회복량: +{potions[i].Value} | 남은 수량: {potions[i].Count}개");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("1. 사용하기");
+                Console.WriteLine("0. 나가기");
+
+                Console.Write("\n원하는 행동을 입력해주세요.\n>>");
+                string input = Console.ReadLine();
+                if (input == "0") return;
+                else if (input == "1")
+                {
+                    if (potions.Count > 0)
+                    {
+                        SelectPotion();
+                    }
+                    else
+                    {
+                        Console.WriteLine("사용할 수 있는 포션이 없습니다!");
+                        Thread.Sleep(1000);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        private void SelectPotion()
+        {
+            if (potions.Count == 1)
+            {
+                UsePotion(potions[0]);
+                return;
+            }
+
+            Console.WriteLine("\n사용할 포션을 선택해주세요. (0: 취소)");
+            Console.Write(">>");
+
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= potions.Count)
+            {
+                UsePotion(potions[index - 1]);
+            }
+            else if (index != 0)
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void UsePotion(Item potion)
+        {
+            if (potion.Count > 0)
+            {
+                if (character.Hp == character.MaxHP)
+                {
+                    Console.WriteLine("이미 체력이 최대입니다!");
+                    Thread.Sleep(1000);
+                    return;
+                }
+
+                character.UsePotion(potion);
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Console.WriteLine("포션이 부족합니다!");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
---
+++ TextRPGTeam33/Stage.cs
@@ -0,0 +1,95 @@
+﻿namespace TextRPGTeam33
+{
+    public class Stage
+    {
+        private Character player;
+        private Inventory inventory;
+        List<Monster> monsterList;
+        List<Item> itemList;
+
+        public int stageFloor { get; set; } = 1;
+
+        public Stage(Character player, Inventory inventory)
+        {
+            this.player = player;
+            this.inventory = inventory;
+
+            //몬스터 리스트
+            monsterList = new List<Monster>
+            {
+                new Monster("미니언", 2, 15, 5),
+                new Monster("공허충", 3, 10, 9),
+                new Monster("대포미니언", 5, 25, 8)
+            };
+
+            //스테이지 클리어 아이템 보상리스트
+            itemList = new List<Item>
+            {
+                new Item("수련자의 갑옷", ItemType.Amor, 4, 3, "수련에 도움을 주는 갑옷입니다.", 1000),
+                new Item("낡은 검", ItemType.Weapon, 5, 3, "쉽게 볼 수 있는 낡은 검 입니다.", 600),
+                new Item("회복 포션", ItemType.Potion, 30, 10, "스파르타의 전사들이 사용했다는 전설의 포션입니다.", 1000)
+            };
+        }
+
+        //전투 시작 전 몬스터 랜덤 생성
+        public List<Monster> CreateMonster()
+        {
+            List<Monster> createMonster = new List<Monster>();
+            Random rand = new Random();
+            int monsterCnt = 3;
+            int monsterId = 0;
+
+            monsterCnt += rand.Next(0, stageFloor); //몬스터 수 랜덤
+            if (monsterCnt > 5) monsterCnt = 5; //몬스터 수 최대 5마리
+
+            //5층마다 보스 몬스터 추가
+            if (stageFloor % 5 == 0)
+            {
+                monsterCnt -= 1;
+                createMonster.Add(new Monster("바론", 10, 30, 10));
+            }
+
+            for (int i = 0; i < monsterCnt; i++)
+            {
+                monsterId = rand.Next(0, 3);   //몬스터 종류 랜덤
+                createMonster.Add(new Monster(monsterList[monsterId].name, monsterList[monsterId].level, monsterList[monsterId].hp, monsterList[monsterId].atk));
+            }
+
+            return createMonster;
+        }
+
+        //전투 종료 후 보상 지급
+        public void StageClear(List<Monster> monsterList)
+        {
+            Random rand = new Random();
+            int exp = 0;
+            int gold = 0;
+            int rewardRate = 0;
+            List<Item> rewardItems = new List<Item>();
+
+            //경험치는 몬스터 레벨 * 5만큼 획득
+            //골드는 몬스터 레벨 * 50의 90 ~ 110% 만큼 획득
+            //보상 아이템 획득 확률은 (아이템별 확률 + (몬스터의 레벨 / 5))
+            foreach (Monster monster in monsterList)
+            {
+                exp += (monster.level * 5);
+                gold += (int)((monster.level * 50) * (1 + (rand.NextDouble() * 0.2 - 0.1)));
+
+                foreach (Item item in itemList)
+                {
+                    rewardRate = item.ItemRate + monster.level / 5;
+                    if (rand.Next(0, 100) < rewardRate)
+                    {
+                        rewardItems.Add(item);
+                        rewardItems.Add(new Item(item.Name, item.Type, item.Value, item.ItemRate, item.Descrip, item.Cost));
+                    }
+                }
+            }
+
+            //보상 지급
+            player.Gold += gold;
+            inventory.AddItem(rewardItems);
+            stageFloor += 1;
+        }
+    }
+}
