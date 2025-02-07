1c7ee71e71158106396ca69a4c049c2e7c3074bf
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-07 11:39:34 +0900

    Revert "Remove: 전투 외 삭제"
    This reverts commit 74f3467ec7c9589a5573912dfecfb395f03ff7db.

---
+++ TextRPGTeam33/Monster.cs
@@ -0,0 +1,17 @@
+﻿namespace TextRPGTeam33
+{
+    public class Monster
+    {
+        public string name { get; }
+        public int level { get; }
+        public int hp { get; set; }
+        public int atk { get; }
+        public Monster(string name, int level, int hp, int atk)
+        {
+            this.name = name;
+            this.level = level;
+            this.hp = hp;
+            this.atk = atk;
+        }
+    }
+}
