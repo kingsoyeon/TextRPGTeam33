ed35ea5747dfb9f8bf4ce683c6654e7eb09e1dd0
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-08 20:55:48 +0900

    Fix: 직업명 변경 및 히든직업 추가
    기존 직업명에서 변경,
    히든 직업을 케이스 6-9에 추가

--- TextRPGTeam33/CharacterCreator.cs
+++ TextRPGTeam33/CharacterCreator.cs
@@ -28,11 +28,11 @@ namespace TextRPGTeam33
 
                 // 직업 입력
                 Console.WriteLine("직업을 선택하세요");
-                Console.WriteLine("1. 전사");
-                Console.WriteLine("2. 기사");
-                Console.WriteLine("3. 마법사");
-                Console.WriteLine("4. 궁수");
-                Console.WriteLine("5. 도적");
+                Console.WriteLine("1. 탈영병");
+                Console.WriteLine("2. 개 조련사");
+                Console.WriteLine("3. 폭발물 산업기사");
+                Console.WriteLine("4. 소방관");
+                Console.WriteLine("5. 이성언 튜터");
                 Console.WriteLine("");
                 Console.WriteLine("원하시는 번호를 선택하세요.");
                 string selectJob = Console.ReadLine();
@@ -50,17 +50,21 @@ namespace TextRPGTeam33
                 
                 switch (selectJob)
                 {
+                        // 기본스탯 높음
+                        // 얼음 속성
                         case "1":
-                        job = "전사";
+                        job = "탈영병";    
                         atk = 10;
                         def = 5;
                         maxHp = 100;
                         maxMp = 30;
                         
                         break;
-
+                        
+                        // 두번 공격, 아이템 물어 오기
+                        // 번개 속성
                         case "2":
-                        job = "기사";
+                        job = "개 조련사";
                         atk = 8;
                         def = 7;
                         maxHp = 110;
@@ -68,8 +72,10 @@ namespace TextRPGTeam33
                         
                         break;
 
+                        // 범위 공격, 본인도 데미지
+                        // 불 속성
                         case "3":
-                        job = "마법사";
+                        job = "폭발물 산업기사";
                         atk = 6;
                         def = 4;
                         maxHp = 70;
@@ -77,33 +83,81 @@ namespace TextRPGTeam33
                         
                         break;
 
+                        // 하이드로펌프, 2명 공격
+                        // 물 속성
                         case "4":
-                        job = "궁수";
+                        job = "소방관";
                         atk = 6;
                         def = 5;
                         maxHp = 80;
                         maxMp = 60;
                        
                         break;
-
+                        
+                        // 파괴광선, 볼트태클
+                        // 바위속성
                         case "5":
-                        job = "도적";
+                        job = "이성언 튜터";
                         atk = 11;
                         def = 6;
                         maxHp = 80;
                         maxMp = 40;
                         
                         break;
+                    
+                    
+                    // 히든 직업
 
-                    default:
+                    // 바위 속성
+                    case "6":
+                    job = "파피루스";
+                    atk = 11;
+                    def = 6;
+                    maxHp = 80;
+                    maxMp = 40;
+
+                    break;
+
+                    // 스킬 없음
+                    // 불 속성
+                    case "7":
+                    job = "대머리백수";
+                    atk = 11;
+                    def = 6;
+                    maxHp = 80;
+                    maxMp = 40;
+                        
+                    break;
+
+                    // 수리검, 나선환
+                    // 번개 속성
+                    case "8":
+                    job = "닌자";
+                    atk = 11;
+                    def = 6;
+                    maxHp = 80;
+                    maxMp = 40;
+
+                    break;
+
+                    // 즉힐, 광전사모드
+                    // 풀 속성
+                    case "9":
+                    job = "의사";
+                    atk = 11;
+                    def = 6;
+                    maxHp = 80;
+                    maxMp = 40;
+
+                    break;
+
+                default:
                         Console.WriteLine("잘못된 번호입니다. 다시 입력하세요.");
                         return null;
                 }
 
                 return new Character(level, name, job, atk, def, maxHp, maxMp, gold);
 
-            
-            
         }
     }
 }
