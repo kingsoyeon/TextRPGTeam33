7787618a8eefe5f7c3c6fa85c02baa790075ef8e
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-09 21:35:47 +0900

    Feat: Skill 클래스
    Skill 클래스 수정

--- TextRPGTeam33/Skill.cs
+++ TextRPGTeam33/Skill.cs
@@ -7,15 +7,27 @@ using System.Threading.Tasks;
 
 namespace TextRPGTeam33
 {
+    public enum SkillType
+    {
+        ATTACK_TWICE,
+        RANGE_ATTACK,
+        HYDRO_PUMP,
+        FIRE_RAY
+    }
+
     public class Skill
     {
-        string name;
-        int mp;
-        int atk;
+        public SkillType type;
+        public string name;
+        public string desc;
+        public int mp;
+        public int atk;
 
-        public Skill(string name, int mp, int atk)
+        public Skill(SkillType type, string name, string desc, int mp, int atk)
         {
+            this.type = type;
             this.name = name;
+            this.desc = desc;
             this.mp = mp;
             this.atk = atk;
         }
