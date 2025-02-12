018cc244590d81e517a95df5a10349f6651989d7
geroer <110544587+geroer@users.noreply.github.com>
2025-02-12 11:05:42 +0900

    Fix: maxHp 추가

--- TextRPGTeam33/Monster.cs
+++ TextRPGTeam33/Monster.cs
@@ -5,6 +5,7 @@
         public string name { get; }
         public int level { get; }
         public int hp { get; set; }
+        public int maxHp { get; set; }
         public int atk { get; }
         public bool isBoss { get; }
         public Monster(string name, int level, int hp, int atk, bool isBoss)
@@ -12,6 +13,7 @@
             this.name = name;
             this.level = level;
             this.hp = hp;
+            this.maxHp = hp;
             this.atk = atk;
             this.isBoss = isBoss;
         }
